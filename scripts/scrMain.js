50e4cc4374d2ab52d2b99e1e32753e4798af2833
wassi <wassi@HPWASSIM>
2025-07-24 14:21:58 +0200

    Load preview and navig

--- scripts/scrMain.js
+++ scripts/scrMain.js
@@ -1,112 +1,3 @@
-// import { createGallery } from "./gallery.js";
-
-function loadNewImgdate(newImgId) {
-  const newImgData = myImgs[newImgId];
-  const newImg = document.getElementById("currentImg");
-  newImg.src = newImgData.img_src;
-  const newDisc = document.getElementById("currentBesch");
-  newDisc.textContent = "";
-  newDisc.textContent = `Naice to Know  \n \n ${newImgData.img_capt} \n\n with ID : ${newImgId} `;
-}
-
-function UpdateObjImg(currentImg) {
-  const rightBtn = document.getElementById("btn_pfail_ra");
-  const leftBtn = document.getElementById("btn_pfail_li");
-  rightBtn.onclick = () => {
-    if (rightBtn) {
-      if (currentImg === myImgs.length - 1) {
-        currentImg = 0;
-        loadNewImgdate(currentImg);
-      } else {
-        currentImg++;
-        loadNewImgdate(currentImg);
-      }
-    }
-  };
-  leftBtn.onclick = () => {
-    if (leftBtn) {
-      if (currentImg === 0) {
-        currentImg = myImgs.length - 1;
-        loadNewImgdate(currentImg);
-      } else {
-        currentImg--;
-        loadNewImgdate(currentImg);
-      }
-    }
-  };
-}
-
-function openPreview(imgData, i) {
-  let currentImg = i;
-  const boxDialog = document.createElement("dialog");
-  boxDialog.classList.add("box_privew");
-  boxDialog.id = "dialog5";
-  const navig = document.createElement("div");
-  navig.id = "navig";
-  navig.classList.add("box_navi");
-  const _imgBig = renderCurrentImage(imgData.img_src);
-  const _rightBtn = createBtnRight();
-  const _leftBtn = createBtnLeft();
-  navig.append(_leftBtn, _imgBig, _rightBtn);
-  const _disc = createDiscription(imgData.img_capt, currentImg);
-  const _btnClose = createBtnClose();
-  boxDialog.append(navig, _disc, _btnClose);
-  document.body.appendChild(boxDialog);
-  _btnClose.onclick = () => closePreview();
-  UpdateObjImg(currentImg);
-  boxDialog.showModal();
-  backToGallery();
-}
-
-function renderCurrentImage(src) {
-  const imgBig = document.createElement("img");
-  imgBig.id = "currentImg";
-  imgBig.alt = "Motorrad image";
-  imgBig.src = src;
-  return imgBig;
-}
-
-function createBtnLeft() {
-  const btnpfail_li = document.createElement("button");
-  btnpfail_li.id = "btn_pfail_li";
-  btnpfail_li.classList.add("btn_pfail");
-  btnpfail_li.type = "button";
-  btnpfail_li.textContent = "<";
-  return btnpfail_li;
-}
-
-function createBtnRight() {
-  const btnpfail_ra = document.createElement("button");
-  btnpfail_ra.id = "btn_pfail_ra";
-  btnpfail_ra.classList.add("btn_pfail");
-  btnpfail_ra.type = "button";
-  btnpfail_ra.textContent = ">";
-  return btnpfail_ra;
-}
-
-function createDiscription(_String, i) {
-  const boxBesch = document.createElement("div");
-  boxBesch.id = "boxBesch";
-  const disc = document.createElement("p");
-  disc.id = "currentBesch";
-  disc.classList.add("beschreibung");
-  disc.textContent = `Naice to Know  \n \n ${_String} \n \n \t\t with ID : ${i}`;
-  boxBesch.append(disc);
-  return boxBesch;
-}
-
-function createBtnClose() {
-  const btnClose = document.createElement("button");
-  btnClose.id = "btn_close";
-  btnClose.classList.add("center");
-  btnClose.type = "button";
-  btnClose.textContent = "BACK TO GALLERY";
-  return btnClose;
-}
-
-/**
- * 1-2-
- */
 async function createMain() {
   const main = document.createElement("main");
   const msg = document.createElement("section");
@@ -124,10 +15,6 @@ async function createMain() {
   return main;
 }
 
-//
-/**
- * 1-
- */
 async function loadContent() {
   const body = document.body;
   //   const header = createHeader();
@@ -136,17 +23,6 @@ async function loadContent() {
   body.append(main, footer);
 }
 
-function closePreview() {
-  const targetPrrview = document.getElementById("dialog5");
-  if (targetPrrview) {
-    targetPrrview.close();
-    targetPrrview.remove();
-  }
-}
-
-/**
- * 1-1
- */
 function createHeader() {
   const header = document.createElement("header");
   header.classList.add("head_container");
@@ -157,21 +33,6 @@ function createHeader() {
   return header;
 }
 
-function backToGallery() {
-  let boxPreview = document.getElementById("dialog5");
-  let besch = document.getElementById("boxBesch");
-  let navi = document.getElementById("navig");
-  boxPreview.onclick = (event) => {
-    if (navi.contains(event.target) || besch.contains(event.target)) {
-      return;
-    }
-    closePreview();
-  };
-}
-
-/**
- * 1-3-
- */
 function createFooter() {
   const footer = document.createElement("footer");
   const footer_div = document.createElement("div");
@@ -192,7 +53,3 @@ function createFooter() {
 }
 
 window.onload = () => loadContent();
-
-/**
- *
- */
